1070c702240826d22c5b791e1990adabe16c9b35
Pham Thi Quynh Anh <quynhanhphamthi01@gmail.com>
2024-05-03 18:01:49 +0700

    init

---
+++ stuman/server/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.2.5"
+	id("io.spring.dependency-management") version "1.1.4"
+	id("org.hibernate.orm") version "6.4.4.Final"
+	id("org.graalvm.buildtools.native") version "0.9.28"
+	id("org.asciidoctor.jvm.convert") version "3.3.2"
+}
+
+group = "sch.stu"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["snippetsDir"] = file("build/generated-snippets")
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.test {
+	outputs.dir(project.extra["snippetsDir"]!!)
+}
+
+tasks.asciidoctor {
+	inputs.dir(project.extra["snippetsDir"]!!)
+	dependsOn(tasks.test)
+}
+
+hibernate {
+	enhancement {
+		enableAssociationManagement.set(true)
+	}
+}
+
